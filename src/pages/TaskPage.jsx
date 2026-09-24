88c5fa4a6a95097925f73feaeaf9071b2532cd46
patricia11 <cassimiro.pati@gmail.com>
2026-09-24 18:09:55 +0100

    Correção de Responsividade

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -12,11 +12,7 @@ function onClickBack() {
 }   
 
 return ( 
-    <div className="h-screen w-screen bg-slate-500 p-6">
-        <div className="w-[500px] space-y-4">
-         
-           
-
+        
             <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
                 <div className="w-[500px] space-y-4">
                  <div className="flex justify-center relative mb-6 text-slate-100">
@@ -37,8 +33,6 @@ return (
                    
                 </div>
             </div>
-        </div>        
-    </div>
 );
 }
 export default TaskPage;
